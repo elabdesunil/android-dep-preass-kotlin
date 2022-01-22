38c614c6e02123ceecedb40e4baf5e0d3a0abb65
unknown <22517595+sunilale0@users.noreply.github.com>
2022-01-22 16:30:15 -0500

    Adding and Removing items

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -7,10 +7,14 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
 
-    val listOfTasks = mutableListOf<String>()
+    var listOfTasks = mutableListOf<String>()
     lateinit var adapter:TaskItemAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +26,18 @@ class MainActivity : AppCompatActivity() {
                 listOfTasks.removeAt(position)
                 // 2. Notify the adapter that our data set has changed
                 adapter.notifyDataSetChanged()
+                saveItems()
             }
 
         }
 
         // 1. Let's detect when the user clicks on the add button
-        findViewById<Button>(R.id.button).setOnClickListener{
-            // Code in here is going to be execute d when the user clicks on a button
-            Log.i("caren", "clicked on... ")
-        }
+//        findViewById<Button>(R.id.button).setOnClickListener{
+//            // Code in here is going to be execute d when the user clicks on a button
+//            Log.i("caren", "clicked on... ")
+//        }
 
-        listOfTasks.add("Do laundry")
-        listOfTasks.add("Go for a walk")
+        loadItems()
 
         // Lookup the recyclerview in activity layout
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView) as RecyclerView
@@ -63,6 +67,39 @@ class MainActivity : AppCompatActivity() {
 
             // 3. Reset text field
             inputTextField.setText("")
+            saveItems()
         }
     }
+
+    // Save the data the user has inputted
+    // by reading from and writing to a file
+
+    // Create a method to get the file we need
+    fun getDataFile(): File {
+
+        // Every line is going to represent a specific task in our list of tasks
+            return File(filesDir, "data.txt")
+    }
+
+    // load the items by reading every line in the data file
+    fun loadItems(){
+        try{
+            listOfTasks = FileUtils.readLines(getDataFile(), Charset.defaultCharset() )
+
+        }catch(ioException:IOException){
+            ioException.printStackTrace()
+        }
+    }
+
+
+    // save items by writing them into our data file
+    fun saveItems(){
+        try{
+            FileUtils.writeLines(getDataFile(), listOfTasks)
+        }catch(ioException:IOException){
+            ioException.printStackTrace()
+        }
+
+    }
+
 }
